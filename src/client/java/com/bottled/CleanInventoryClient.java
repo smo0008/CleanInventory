4e2801214b021d04e4d0f9b87a04dff76190464a
bottled <ree.ruben@proton.me>
2025-09-29 19:34:15 +1000

    Initial commit

---
+++ src/client/java/com/bottled/CleanInventoryClient.java
@@ -0,0 +1,10 @@
+package com.bottled;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CleanInventoryClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
